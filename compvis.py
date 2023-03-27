04912f2303ac236a4c6027015822200d0e9ce542
Aleem Anwar <91719374+aleemanwar12@users.noreply.github.com>
2023-03-27 09:56:39 +0530

    rename the 'face_detection.xml' according to the file name you have downloaded

--- compvis.py
+++ compvis.py
@@ -1,32 +1,23 @@
 import cv2
-import numpy as np
-
-def boundary_of_image(img, classifier, scaleFactor, minNeighbours, color, text):
-    grayscale_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    features = classifier.detectMultiScale(grayscale_img, scaleFactor, minNeighbours)
-    coords = []
-    for (x, y, w, h) in features:
-        cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-        cv2.putText(img, text, (x, y - 4), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2, cv2.LINE_AA)
-        coords = [x, y, w, h]
-        print(coords)
-        return coords, img
-
-faceCasade = cv2.CascadeClassifier("face_detection.xml")
-def detect(img, faceCasade):
-    color = {"blue":(255, 0, 0), "red":(0, 0, 255), "green":(0,255,0)}
-    coords = boundary_of_image(img, faceCasade, 1.1, 11, color['green'], "Face")
-    return img
-
+import serial
+import time
+arduino = serial.Serial('COM3', 9600)
+cascade = cv2.CascadeClassifier('face_detection.xml')
 
 cap = cv2.VideoCapture(0)
-
 while True:
-    _, imge = cap.read()
-    imge = detect(imge, faceCasade)
-    cv2.imshow("Webcam", imge)
-    print()
+    _, img = cap.read()
+    grayscale = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    face_detect = cascade.detectMultiScale(grayscale, 1.5, 5)
+    for (x, y, w, h) in face_detect:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        centerX = int((x + w/2) * 180 / img.shape[1])
+        centerY = int((y + h/2) * 180 / img.shape[0])
+        print(centerX, centerY)
+        arduino.write(f'{centerX}, {centerY}\n'.encode())
+        cv2.imshow("Webcam", img)
+        cv2.imshow("gray_webcam", grayscale)
+        time.sleep(0.1)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-cap.release()
-cv2.destroyAllWindows()
